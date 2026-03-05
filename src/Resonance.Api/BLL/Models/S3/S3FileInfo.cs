b826f53275d22552b0feae9a968e0a95a584cb48
DVV_COMPUTER\biker <dvv153m@gmail.com>
2026-03-05 22:48:53 +0300

    add s3 models

---
+++ src/Resonance.Api/BLL/Models/S3/S3FileInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace Resonance.Api.BLL.Models.S3
+{
+    public class S3FileInfo
+    {
+        public string Name { get; set; }
+        public string BucketName { get; set; }
+        public DateTime LastModified { get; set; }
+        public long Size { get; set; }
+        public string ETag { get; set; }
+        public string ContentType { get; set; }
+    }
+}
---
+++ src/Resonance.Api/BLL/Models/S3/S3OperationResult.cs
@@ -0,0 +1,24 @@
+﻿namespace Resonance.Api.BLL.Models.S3
+{
+    public class S3OperationResult<T>
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public T Data { get; set; }
+        public string ErrorCode { get; set; }
+
+        public static S3OperationResult<T> Ok(T data, string message = null) => new()
+        {
+            Success = true,
+            Data = data,
+            Message = message
+        };
+
+        public static S3OperationResult<T> Fail(string error, string code = null) => new()
+        {
+            Success = false,
+            Message = error,
+            ErrorCode = code
+        };
+    }
+}
